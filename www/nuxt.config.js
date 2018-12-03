6151925a5e66dbdb9ee08f1bf656eb7aa299d521
Mackenzie Kieran <mackenzie@redacademy.com>
2018-12-03 13:16:04 -0800

    Adjust now build config.

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  mode: 'spa',
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  loading: { color: '#fff' },
+
+  // MODULES
   modules: ['@nuxtjs/apollo'],
   apollo: {
     clientConfigs: {
